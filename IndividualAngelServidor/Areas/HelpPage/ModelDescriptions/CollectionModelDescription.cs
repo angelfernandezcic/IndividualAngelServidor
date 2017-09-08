36f2f50405d965a41fb0d5aae67189662973e7be
usuario <angelfernandezcic@gmail.com>
2017-09-08 15:51:19 +0200

    Add project files.

---
+++ IndividualAngelServidor/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace IndividualAngelServidor.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
